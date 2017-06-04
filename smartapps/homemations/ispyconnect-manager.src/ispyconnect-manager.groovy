9d1353d61809fa86fa9193474637de8cda448f85
Stephen Harris <stephennharris@bellsouth.net>
2017-06-04 11:53:22 -0400

    Version 1.0 - Working and testing on this since 12/2016.
    Integration with iSpyConnect that requires a subscription to connect any cameras.  Requires the two device types; iSpy Camera and iSpy PTZ Camera.  Also a API key and access token that can be acquired from https://www.ispyconnect.com/API.aspx#/page/login
    
    Looking for feedback.  A few functions not working 100%.  Shared for those that have cameras not compatible with Smarththings.  This opens up all cameras supported by iSpyConnect.

--- smartapps/homemations/ispyconnect-manager.src/ispyconnect-manager.groovy
+++ smartapps/homemations/ispyconnect-manager.src/ispyconnect-manager.groovy
@@ -1,7 +1,7 @@
 /** 
-*  iSpyConnect Manager 
+*  iSpyConnect Manager by Stephen Harris (stephen@homemations.com) 
 *   
-*  Copyright 2016 Stephen Harris 
+*  Copyright 2016 Homemations, Inc 
 * 
 *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except 
 *  in compliance with the License. You may obtain a copy of the License at: 
